475d3bdb76b0bae93f3547c8cd6fccb9a2f2dca9
Zernel <itzernel@gmail.com>
2011-05-20 16:42:17 +0800

    Start the Pro_EasyBBS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easybbs::Application.config.secret_token = 'bd188e2984da14938e8d858acf66334f115471566759d036bca7ed462a93c058addb8b0f358e44e20cba28fd104f82da3ae984705144ce00a147ead0d106d773'
